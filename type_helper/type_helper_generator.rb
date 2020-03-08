2e93723580a001060fda578543b756d57bd7023b
Luca <lucamoro96@gmail.com>
2020-03-08 20:40:40 +0100

    removed policy generation

--- type_helper/type_helper_generator.rb
+++ type_helper/type_helper_generator.rb
@@ -11,7 +11,6 @@ class Rails::TypeHelperGenerator < Rails::Generators::NamedBase
 
   class_option :no_type, type: :boolean, default: false, desc: "Skip types generation"
   class_option :no_enums, type: :boolean, default: false, desc: "Skip enums generation"
-  class_option :no_policy, type: :boolean, default: false, desc: "Skip policy generation"
   class_option :no_input, type: :boolean, default: false, desc: "Skip input generation"
   class_option :no_mutations, type: :boolean, default: false, desc: "Skip mutations generation"
   class_option :no_dependencies, type: :boolean, default: false, desc: "Skip dependencies generation"
@@ -69,46 +68,19 @@ class Rails::TypeHelperGenerator < Rails::Generators::NamedBase
   def create_create_create_mutation_file
     return if options["no_mutations"]
 
-    @resolver_head = options["no_policy"] ? "" : "Pundit.authorize(current_user, #{class_name}, :create?)"
     template "mutation_create_template.erb", "app/graphql/mutations/#{file_name}_mutations/#{file_name}_create.rb"
   end
 
   def create_update_mutation_file
     return if options["no_mutations"]
 
-    @resolver_head = options["no_policy"] ? "#{file_name} = #{class_name}.find(#{file_name}_id)" \
-                    : "#{file_name} = Pundit.authorize(current_user, #{class_name}.find(#{file_name}_id), :update?)"
     template "mutation_update_template.erb", "app/graphql/mutations/#{file_name}_mutations/#{file_name}_update.rb"
   end
 
   def create_delete_mutation_file
     return if options["no_mutations"]
 
-    @resolver_head = options["no_policy"] ? "#{file_name} = #{class_name}.find(#{file_name}_id)" \
-                    : "#{file_name} = Pundit.authorize(current_user, #{class_name}.find(input[:#{file_name}_id]), :delete?)"
     template "mutation_delete_template.erb", "app/graphql/mutations/#{file_name}_mutations/#{file_name}_delete.rb"
-  end
-
-  def create_policy_file
-    return if options["no_policy"]
-    create_file "app/policies/#{file_name}_policy.rb", <<-FILE
-class #{class_name}Policy < ApplicationPolicy
-  # TODO implement  
-
-  def create?
-    false
-  end
-
-  def update?
-    false
-  end
-
-  def delete?
-    false
-  end
-
-end
-    FILE
   end
 
   def get_dependencies
@@ -123,8 +95,6 @@ end
       d_file_name = dependency.tableize.singularize
       # check type
       missing_type = !File.exist?("app/graphql/types/#{d_file_name}_type.rb")
-      # check policy
-      missing_policy = !File.exist?("app/policies/#{d_file_name}_policy.rb")
       # check input
       missing_input = !File.exists?("app/graphql/types/inputs/#{d_file_name}_input.rb")
       # check mutations
@@ -132,7 +102,7 @@ end
          || !File.exist?("app/graphql/mutations/#{d_file_name}_mutations/#{d_file_name}_update.rb") \
          || !File.exist?("app/graphql/mutations/#{d_file_name}_mutations/#{d_file_name}_delete.rb")
 
-      if missing_type || missing_policy || missing_input || missing_mutations
+      if missing_type || missing_input || missing_mutations
         if yes?("Found unresolved dependency for: #{dependency}. Do you want to generate it?")
           args = dependency.to_s + @options.map{ |k, v| " --#{k.to_s} #{v.to_s}" }.join
           generate :type_helper, args
@@ -143,16 +113,6 @@ end
     end
   end
 
-  def check_application_policy
-    return if options["no_policy"] or options["no_dependencies"]
-
-    unless File.exist?("app/policies/application_policy.rb")
-      if yes? "The generated policies are based on the base class ApplicationPolicy and it seems you don't have it in your project. Do you want to generate it?"
-        copy_file "application_policy.rb", "app/policies/application_policy.rb"
-      end
-    end
-
-  end
 
   def check_operations_return_type
     return if options["no_mutations"] or options["no_dependencies"]
